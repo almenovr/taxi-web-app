945bbd67f6d072fdafb82c2fc7b88e8591fcdbaa
Ruslan <7@azfIz@>
2025-02-22 13:24:37 +0300

    fix: destinations

--- server-side/types/generated/components.d.ts
+++ server-side/types/generated/components.d.ts
@@ -9,7 +9,7 @@ export interface SharedCarBlock extends Struct.ComponentSchema {
   attributes: {
     carImg: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     className: Schema.Attribute.String;
-    description: Schema.Attribute.Blocks;
+    description: Schema.Attribute.RichText;
     isBusiness: Schema.Attribute.Boolean;
     listCars: Schema.Attribute.String;
     price: Schema.Attribute.Integer;
